e1df156379db440f90ca51a5e86d0b77ed3dfdd3
Roman K <roman@obraz.es>
2019-02-10 18:52:31 +0200

    Init: project configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require html-webpack-plugin plugin
+var ExtractTextPlugin = require('extract-text-webpack-plugin'); // Require extract text plugin
+
+module.exports = {
+  entry: __dirname + "/src/index.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: __dirname + '/dist', // Folder to store generated bundle
+    filename: 'bundle.js',  // Name of generated bundle after build
+    publicPath: '/' // public URL of the output directory when referenced in a browser
+  },
+  module: {  // where we defined file patterns and their loaders
+    rules: [ 
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: [
+          /node_modules/
+        ]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [  // Array of plugins to apply to build chunk
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      inject: 'body'
+    }),
+    new ExtractTextPlugin("styles.css"), // extract css to a separate file called styles.css
+  ],
+  devServer: {  // configuration for webpack-dev-server
+    contentBase: './src/public',  //source of static assets
+    port: 8080, // port to run dev-server
+  } 
+};
